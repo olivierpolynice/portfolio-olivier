58e5d1ff09fc816135bdd8778a99e65a44277bee
Olivier Polynice <olivierpolynice7@gmail.com>
2026-08-11 15:06:39 +0200

    Ajout de Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
+import App from './App.jsx'
 import './index.css'
 import './styles/components.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
